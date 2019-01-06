1f48c682449ab4acfc2d752fb8dced1a6a95de43
Sean Coonce <sean@bitgo.com>
2019-01-05 16:23:19 -0800

    Update w/ real lambda (last commit was bunk)

--- lambda.py
+++ lambda.py
@@ -1,58 +1,37 @@
-MAINNET_KEY_NAME = 'mainnet'
-TESTNET_KEY_NAME = 'testnet'
-BG_URL = 'bg_url'
-
+import datetime
 import json
 
-from botocore.vendored import requests
+from urllib.request import Request, urlopen
+
+import boto3
 
 
-COIN_INDEXER_MAPPING = {
-    'BTC': {
-        MAINNET_KEY_NAME: {
-            BG_URL: 'https://www.bitgo.com/api/v2/btc/public/block/latest',
-            'handlers': [
-                'handlers.sochain.sochainApiHandler',
-            ]
+def lambda_handler(event, context):
+    """
+    Runs through every indexer in BitGo's stack, compares it state to a public
+    block explorer, and writes that state to a JSON file on s3 (actually, two
+    JSON files, one time-stamped file and another that represents the "latest"
+    file that a front-end app will pull from.)
+    """
+    current_time = datetime.datetime.now()
+
+    indexer_data = {
+        'BTC': {
+            'foo': 'bar',
         },
-        TESTNET_KEY_NAME: {
-            BG_URL: 'https://test.bitgo.com/api/v2/tbtc/public/block/latest',
-            'handlers': [
-                'handlers.sochain.sochainApiHandler',
-            ]
+        'LTC': {
+            'foo': 'bzrtz'
         }
-    },
-}
+    }
+    string = json.dumps(indexer_data)
+    encoded_string = string.encode("utf-8")
 
-# Used to populate the status of each indexer the app checks
-# Eventually, the datastructure will look like:
-# {
-#   'BTC': {
-#     'mainnet': {
-#       'status': True/False,
-#       'latest_block': 23421,
-#       'blocks_behind': 0,
-#     }
-#   }
-# }
-INDEXER_STATUS = {}
+    # Create the file names
+    dated_file_name = "{}.json".format(current_time)
+    latest_file_name = "latest.json"
 
-for coin, config in COIN_INDEXER_MAPPING.iteritems():
-    print coin
-    response = requests.get(config[MAINNET_KEY_NAME][BG_URL])
-    json_response = json.loads(response.content)
-
-    # Sometimes the IMS response will return the fact that it's at chainhead.
-    # If so, trust it (ie: do not compare block height against a public block
-    # explorer) and move on
-    if res.get('chainHead', False):
-        INDEXER_STATUS.setdefault(coin, {})
-        INDEXER_STATUS[coin][MAINNET_KEY_NAME]
-
-
-
-
-class BaseHandler(object):
-    """
-    Handles
-    """
+    # Write the files to the bucket
+    bucket_name = "bitgo-indexer-health"
+    s3 = boto3.resource("s3")
+    s3.Bucket(bucket_name).put_object(Key=dated_file_name, Body=encoded_string)
+    s3.Bucket(bucket_name).put_object(Key=latest_file_name, Body=encoded_string)
